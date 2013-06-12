646d69d54ef8edb61b6315585d6f4118abb8361a
Teacher <Teacher@Teacher-PC>
2013-06-12 11:28:00 -0400

    testing

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -14,6 +14,6 @@ public class HelloGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello, Git!!!");
+        System.out.println("Hello, Git!!!!");
     }
 }
